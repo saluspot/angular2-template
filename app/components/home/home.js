dcc7b99e585d62fe835668b084a69cd0f8cc2bcd
Eduardo <choboellobo@gmail.com>
2016-06-29 09:57:16 +0200

    Angular2 Base Template Hello World rc3

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -1,4 +1,4 @@
-System.register(['angular2-chartist', "@angular/core"], function(exports_1, context_1) {
+System.register(["@angular/core"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,35 +10,21 @@ System.register(['angular2-chartist', "@angular/core"], function(exports_1, cont
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var angular2_chartist_1, core_1;
+    var core_1;
     var Home;
     return {
         setters:[
-            function (angular2_chartist_1_1) {
-                angular2_chartist_1 = angular2_chartist_1_1;
-            },
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
             Home = (function () {
                 function Home() {
-                    this.data = {
-                        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-                        series: [
-                            [12, 9, 7, 8, 5],
-                            [2, 1, 3.5, 7, 3],
-                            [1, 3, 4, 5, 6]
-                        ]
-                    };
-                    this.type = "Line";
-                    this.options = null;
                 }
                 Home = __decorate([
                     core_1.Component({
                         selector: "app",
-                        template: "\n  <h1>Mi app</h1>\n  <chartist\n     [data]=\"data\"\n     [type]=\"type\"\n     [options]=\"options\"\n     >\n   </chartist>\n  ",
-                        directives: [angular2_chartist_1.ChartistComponent]
+                        template: "\n  <h1>Mi app</h1>\n\n  ",
                     }), 
                     __metadata('design:paramtypes', [])
                 ], Home);
--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,34 +1,11 @@
-import { ChartistComponent } from 'angular2-chartist';
 import {Component} from "@angular/core"
-
-
 @Component({
   selector: "app",
   template : `
   <h1>Mi app</h1>
-  <chartist
-     [data]="data"
-     [type]="type"
-     [options]="options"
-     >
-   </chartist>
+
   `,
-  directives:[ChartistComponent]
 })
 export class Home {
-  data:any;
-  type:any;
-  options:any
-  constructor(){
-    this.data =  {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-        series: [
-          [12, 9, 7, 8, 5],
-          [2, 1, 3.5, 7, 3],
-          [1, 3, 4, 5, 6]
-        ]
-      }
-    this.type = "Line"
-    this.options = null
-  }
+
 }
